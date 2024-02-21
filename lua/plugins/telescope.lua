81beb2975cc9534f317e99b4cb8d0415801294ee
oosiriiss <oosiriiss@gmail.com>
2024-02-21 22:40:58 +0100

    harpoon and telescope keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
 	      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 	      vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 	      vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-	      vim.keymap.set('n', '<leader>pB', builtin.buffers, {})
+	      vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 	      vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
 
 	      end
