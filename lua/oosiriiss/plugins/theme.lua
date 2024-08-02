1b0ae02034629578bd7f68b91f446fc4afb5bb12
oosiriiss <oosiriiss@gmail.com>
2024-08-02 18:04:50 +0000

    adde rosepine config

--- lua/oosiriiss/plugins/theme.lua
+++ lua/oosiriiss/plugins/theme.lua
@@ -3,14 +3,28 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	opts = {
+		variant = "main",  -- auto, main, moon, or dawn
+		dark_variant = "main", -- main, moon, or dawn
+		dim_inactive_windows = false,
+		extend_background_behind_borders = true,
 
-		transparency = true,
+		enable = {
+			terminal = true,
+			legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+			migrations = true,  -- Handle deprecated options automatically
+		},
+
+		styles = {
+			bold = true,
+			italic = true,
+			transparency = false,
+		},
 
 	},
 	config = function(_, opts)
 		require('rose-pine').setup(opts)
 
-		vim.cmd('colorscheme rose-pine-moon')
+		vim.cmd('colorscheme rose-pine')
 	end
 
 
